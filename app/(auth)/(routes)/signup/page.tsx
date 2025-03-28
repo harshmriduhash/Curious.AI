dd0a73599cbaca5391f2f7123b8c57cb86fce0d3
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-28 12:35:58 +0530

    Signup

--- app/(auth)/(routes)/signup/page.tsx
+++ app/(auth)/(routes)/signup/page.tsx
@@ -1,8 +1,6 @@
-import { Signup } from '@/components/auth/signup'
-import React from 'react'
+import { Signup } from "@/components/auth/signup";
+import React from "react";
 
 export default function SignUpPage() {
-    return (
-        <Signup />
-    )
+  return <Signup />;
 }
